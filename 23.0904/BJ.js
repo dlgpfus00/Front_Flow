d7eeddef6842b0eae73169a864a915c3d3e03dfd
lyeons <dlgpfus00@gmail.com>
2023-09-22 23:38:57 +0900

    플로우 커밋

---
+++ 23.0904/BJ.js
@@ -0,0 +1,4 @@
+let a = prompt("숫자입력");
+let b = prompt("숫자입력");
+
+alert(a + b);
---
+++ JS/codeFestival.js
@@ -0,0 +1,89 @@
+// //1
+// const nums = [100, 200, 300, 400, 500];
+// const num_del = nums.splice(0, 3);
+// console.log(num_del);
+
+// //2
+// const arr = [200, 100, 300];
+// arr.splice(2, 0, "10000");
+// console.log(arr); //[200,100,10000,300]
+
+// //3
+// const arr = [100, 200, 300];
+// console.log(typeof arr); //object
+
+// //5
+// let a = 10;
+// let b = 2;
+
+// for (let i = 1; i < 5; i += 2) {
+//   a += i;
+// }
+
+// console.log(a + b);999999999999
+
+// //8
+// const d = {
+//   height: 180,
+//   weight: 78,
+//   weight: 84,
+//   temperature: 36,
+//   eyesight: 1,
+// };
+
+// console.log(d["weight"]);
+
+// //9
+// const year = "2019";
+// const month = "04";
+// const day = "26";
+// const hour = "11";
+// const minute = "34";
+// const second = "27";
+
+// const result = year.concat(
+//   "/",
+//   month,
+//   "/",
+//   day + " " + hour,
+//   ":",
+//   minute,
+//   ":",
+//   second
+// );
+// console.log(result);
+
+// //10;
+// let num = 5;
+// for (i = 1; i <= num; i++) {
+//   console.log(" ".repeat(num - i) + "*".repeat(i * 2 - 1));
+// }
+
+// //11
+// let s = 0;
+// for (i = 1; i <= 100; i += 1) {
+//   s += i;
+// }
+// console.log(s);
+
+// //12
+
+// const x = new wizard(545, 210, 10);
+// console.log(x.helth, x.mama, x.armor);
+// x.attack();
+
+// //13
+// let planet = [
+//   "수성",
+//   "금성",
+//   "지구",
+//   "화성",
+//   "목성",
+//   "토성",
+//   "천왕성",
+//   "해왕성",
+// ];
+// const num = 1;
+// console.log(planet[num - 1]);
+
+//14
